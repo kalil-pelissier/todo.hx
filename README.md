39bf65f14a64ae1b5003204f4a51858d8da90b9a
kvlil <kalil.pelissier@gmail.com>
2026-08-05 19:27:32 +0200

    Initial commit
    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# todo.hx
+A plugin for Helix that highlights, lists, and navigates TODO/FIXME comments in your codebase.
---
+++ cog.scm
@@ -0,0 +1,6 @@
+(define package-name 'todo.hx)
+
+(define version "0.1.0")
+(define dependencies '())
+
+(define entrypoint '(#:name "todo.hx" #:path "main.scm"))
